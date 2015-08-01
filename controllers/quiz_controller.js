6df73be57942d6431352fe5f2b2ccbc43fc0e902
LFunes <luisfuness@gmail.com>
2015-08-01 13:34:06 +0200

    Solución errores módulo anterior
    Solución error “errors is not defined”

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -46,7 +46,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', {quiz:req.quiz});
+  res.render('quizes/show', {quiz:req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
