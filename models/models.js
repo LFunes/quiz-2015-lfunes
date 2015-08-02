aef377b752c6744c82d43c5984815d96bfe7f384
LFunes <luisfuness@gmail.com>
2015-08-02 12:12:49 +0200

    Indice temático (1)
    Introducción en el modelo de tema de pregunta y prueba de carga del
    array de temas en la página /quizes

--- models/models.js
+++ models/models.js
@@ -57,11 +57,13 @@ sequelize.sync().then(function(){
     if(count === 0){
         Quiz.create({
           pregunta: 'Capital de Italia',
-          respuesta: 'Roma'
+          respuesta: 'Roma',
+          tema:'Ocio'
         });
         Quiz.create({
           pregunta: 'Capital de Portugal',
-          respuesta: 'Lisboa'
+          respuesta: 'Lisboa',
+          tema:'Ciencia'
         })
         .then(function(){console.log('Base de datos inicializada')});
     }
